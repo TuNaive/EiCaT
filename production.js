55f2ce49a2db5d15348053808f6c427398d9c1fc
luventa <silentmic@qq.com>
2017-12-07 23:25:01 +0800

    fix production.js

--- production.js
+++ production.js
@@ -1,9 +1,11 @@
 const Application = require('thinkjs')
 const babel = require('think-babel')
+const watcher = require('think-watcher')
 const notifier = require('node-notifier')
 
 const instance = new Application({
   ROOT_PATH: __dirname,
+  watcher: watcher,
   proxy: true, // use proxy
   transpiler: [babel, {
     presets: ['think-node']
