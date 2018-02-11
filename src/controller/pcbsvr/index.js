5f506a5fb28c56c30d52f172cda658c717a94024
xuanyanghou <375812401@qq.com>
2018-02-11 16:10:41 +0800

    首页展示

--- src/controller/pcbsvr/index.js
+++ src/controller/pcbsvr/index.js
@@ -1,11 +1,11 @@
 import Base from './base.js'
 
-const LIST_MOCK_DATA = [
+const LIST = [
   {
     "id": 226,
     "uid": 1,
     "name": "",
-    "title": "简约卧室吊顶装修效果图大全",
+    "title": "IC板",
     "category_id": 83,
     "group_id": 0,
     "description": "简约卧室吊顶装修效果图大全简约卧室吊顶装修效果图大全简约卧室吊顶装修效果图大全",
@@ -31,13 +31,14 @@ const LIST_MOCK_DATA = [
     "price": "",
     "sort_id": 7,
     "keyname": "0",
-    "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    // "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//IC板_201612091858234604.jpg"
   },
   {
     "id": 225,
     "uid": 1,
     "name": "",
-    "title": "主卧室吊顶装修效果图大全",
+    "title": "双面绿油软板",
     "category_id": 83,
     "group_id": 0,
     "description": "主卧室吊顶装修效果图大全",
@@ -63,13 +64,14 @@ const LIST_MOCK_DATA = [
     "price": "",
     "sort_id": 7,
     "keyname": "0",
-    "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    // "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-5_201612091856501404.jpg"
   },
   {
     "id": 224,
     "uid": 1,
     "name": "",
-    "title": "主卧室吊顶装修效果图大全",
+    "title": "八层蓝油板",
     "category_id": 83,
     "group_id": 0,
     "description": "主卧室吊顶装修效果图大全主卧室吊顶装修效果图大全主卧室吊顶装修效果图大全",
@@ -95,13 +97,14 @@ const LIST_MOCK_DATA = [
     "price": "",
     "sort_id": 7,
     "keyname": "0",
-    "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    // "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-1_201612091856032011.jpg"
   },
   {
     "id": 223,
     "uid": 1,
     "name": "",
-    "title": "简约室内客厅电视背景墙图片大全",
+    "title": "汽车板",
     "category_id": 83,
     "group_id": 0,
     "description": "简约室内客厅电视背景墙图片大全",
@@ -127,14 +130,69 @@ const LIST_MOCK_DATA = [
     "price": "",
     "sort_id": 7,
     "keyname": "0",
-    "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    // "src": "//odhs9iog7.qnssl.com/bMBnWf9_8i1G1s-OrS7105M8.jpg?imageView2/1/w/600/h/399"
+    "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-2_201612091900351988.jpg"
   }
 ]
 
+const PCB_LIST = [{
+  "id": 300,
+  "title": "8层蓝油板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-1_201612091856032011.jpg"
+}, {
+  "id": 301,
+  "title": "六层套板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-4_201612091905015577.jpg"
+}, {
+  "id": 302,
+  "title": "双面灰油板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-3_201612091901509957.jpg"
+}, {
+  "id": 303,
+  "title": "汽车板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-2_201612091900351988.jpg"
+}];
+const FPC_LIST = [{
+  "id": 304,
+  "title": "双面绿油软板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-5_201612091856501404.jpg"
+}, {
+  "id": 305,
+  "title": "单面软板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-1_201612091859564981.jpg"
+}, {
+  "id": 306,
+  "title": "双面软板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-4_201612091901202621.jpg"
+}, {
+  "id": 307,
+  "title": "四层软硬结合板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-2_201612091902474891.jpg"
+}];
+const PCBA_LIST = [{
+  "id": 308,
+  "title": "IC板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//IC板_201612091858234604.jpg"
+}, {
+  "id": 309,
+  "title": "BGA板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-3_201612091859060430.jpg"
+}, {
+  "id": 310,
+  "title": "SMT",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-4_201612091903353600.jpg"
+}, {
+  "id": 311,
+  "title": "RFID板",
+  "src": "http://fs.digiic.com/download/DownLoadFile?token=&filepath=upload/FreeFile/20161209//未标题-5_201612091904373819.jpg"
+}];
 
 export default class extends Base {
   indexAction() {
-  	this.assign('list', LIST_MOCK_DATA);
-    return this.display()
+  	this.assign('list', LIST);
+    this.assign('pcbList', PCB_LIST);
+    this.assign('fpcList', FPC_LIST);
+    this.assign('pcbaList', PCBA_LIST);
+    return this.display();
   }
 }
