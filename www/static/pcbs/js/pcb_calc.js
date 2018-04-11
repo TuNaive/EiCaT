1dc41dc824420135dab0e0ebb06501072dffd907
yuyingdeng <yuyingdeng@ctrip.com>
2018-04-11 12:21:23 +0800

    待测试： 1. 汇率转化 2. 手机邮箱二选一

--- www/static/pcbs/js/pcb_calc.js
+++ www/static/pcbs/js/pcb_calc.js
@@ -160,13 +160,18 @@ function bindFormEvents () {
     },
     onFinishing: function (event, currentIndex) {
       var formValidRes = $("#pcbFileForm").valid()
+
+      // 手机或邮箱二选一判断
+      var hasMobileOrEmail = $('#mobile').val().trim() || $('#email').val().trim()
+      $('#mobile').next('.error').toggle(!hasAddress);
+      
+      // 收货人信息判断      
       var hasAddress = $('[name="address_id"]:checked').length > 0
-      // 收货人信息判断
       if (!hasAddress) {
         $('#addressErr').css({display: 'inline-block'}).removeClass('hide');
       }
 
-      return formValidRes && hasAddress;
+      return formValidRes && hasMobileOrEmail && hasAddress;
     },
     onFinished: function (event, currentIndex) {
       var paramData = _.concat(
