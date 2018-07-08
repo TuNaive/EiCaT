7504c662d0d33a71d93a4387ccc34d7dab1bc080
luventa <luventa@outlook.com>
2018-07-08 21:15:12 +0800

    fix active bug

--- src/controller/home/scm.js
+++ src/controller/home/scm.js
@@ -2,7 +2,7 @@ import Base from './base'
 
 export default class extends Base {
   indexAction() {
-    this.meta_title = this.__.jed.dgettext('common', 'home')
+    this.meta_title = this.__.jed.dgettext('common', 'scm')
     this.active = ['/scm', '/scm.html']
     return this.display()
   }
