40e707b04b2602219b4242505bd31f03056a6278
luventa <luventa@outlook.com>
2018-11-21 20:47:49 +0800

    fix: FPC file upload

--- src/controller/fpc/upload.js
+++ src/controller/fpc/upload.js
@@ -2,6 +2,11 @@ import Base from './base.js'
 import fs from 'mz/fs'
 
 export default class extends Base {
+  constructor(ctx) {
+    super(ctx)
+    this.uploadPath = think.ROOT_PATH + '/uploadFiles'
+  }
+
   __before() {
     return super.__before(true).then(data => {
       return data
